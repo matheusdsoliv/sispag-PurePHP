dc009454c30f8571251998e043199aac9e865149
Matheus Oliveira <matheus.oliveira@lais.huol.ufrn.br>
2019-06-21 16:17:39 -0300

    alteração branch titulo

--- ApresentaçãoCupomSalarialView.php
+++ ApresentaçãoCupomSalarialView.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
         crossorigin="anonymous">
 
-    <title> Grupo 1 </title>
+    <title> Grupo 1 é o melhor </title>
 </head>
 
 <body>
