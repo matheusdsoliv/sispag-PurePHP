f0fb8767614584386f11ec70c32a7e19bcfc0089
Matheus Oliveira <matheus.oliveira@lais.huol.ufrn.br>
2019-06-21 15:55:53 -0300

    título mudado

--- ApresentaçãoCupomSalarialView.php
+++ ApresentaçãoCupomSalarialView.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
         crossorigin="anonymous">
 
-    <title> Cupom Salarial </title>
+    <title> Grupo 1 </title>
 </head>
 
 <body>
